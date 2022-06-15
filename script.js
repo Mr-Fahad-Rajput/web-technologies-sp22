ca214046c998b27b96866958d5f9104e49518d70
MrFahadRajput <94325634+Mr-Fahad-Rajput@users.noreply.github.com>
2022-06-15 16:43:25 +0500

    Working J Query Todo

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-{}
+$(function(){
+// Binding Function
+
+$("#btnAdd").click(handleButton);
+$("#list li").click(handleDelete);
+console.log("Bindings Successful ");
+
+});
+
+function handleButton(){
+
+    var value = $("#text").val();
+    $("#list").append("<li>" + value + "</li>");
+    $("#list li").click(handleDelete);
+
+};
+
+function handleDelete(){
+
+    $(this).remove();
+
+};
